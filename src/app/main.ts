e7f2051c44dfef46c25559b81a2a8d8ede263f87
ivanmayes <ivanmayes@gmail.com>
2017-10-18 13:02:48 -0500

    Bumped version

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,10 @@
+import { environment } from './../environment/environment';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app.module';
+
+if (environment.production) {
+	enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
